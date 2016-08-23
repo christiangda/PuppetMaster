4c564e344e933b21307763f240a26a23b860fe83
christian <christiangda@gmail.com>
2016-08-23 15:39:29 +0100

    fixed puppet master install

--- Vagrant/scripts/bootstrap.sh
+++ Vagrant/scripts/bootstrap.sh
@@ -3,7 +3,7 @@
 # Prepare the system
 yum -y update
 yum -y install vim htop elinks
-rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
+rpm -Uvh https://yum.puppetlabs.com/puppetlabs-release-pc1-el-7.noarch.rpm
 
 # Enter like root all time
 cat << __EOF__ >> /home/vagrant/.profile
@@ -17,15 +17,15 @@ chown vagrant.vagrant /home/vagrant/.profile
 # Set hostname file (replaced)
 cat << __EOF__ > /etc/hosts
 # MANAGED BY boostrap.sh script in Vagrant
-127.0.0.1   localhost
-10.0.2.2    puppetmaster.local puppetmaster
-10.0.2.3    agent01.local agent01
-
+127.0.0.1         localhost
+192.168.33.100    master.puppet.local master
+192.168.33.101    agent01.puppet.local agent01
+192.168.33.102    agent02.puppet.local agent02
 __EOF__
 
 # Set hostname file (add lines)
 cat << __EOF__ >> /etc/resolv.conf
 # MANAGED BY boostrap.sh script in Vagrant
-domain local
-search local
+domain puppet.local
+search puppet.local
 __EOF__
